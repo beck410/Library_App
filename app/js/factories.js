81837622bdbad70b953b0816e63283ca2eaec3c1
beck410 <beck410@hotmail.com>
2014-12-11 21:40:19 -0600

    moved authFactory to factories.js file

--- app/js/factories.js
+++ app/js/factories.js
@@ -92,4 +92,57 @@
         defaultBook: defaultBook
       };
     })
+    .factory('authFactory', function(FIREBASE_URL){
+      var factory = {},
+      ref = new Firebase(FIREBASE_URL);
+
+      factory.loginUser = function(email,password,cb){
+        ref.authWithPassword({
+          email: email,
+          password: password
+        }, function(error, authData){
+          if(error === null){
+            console.log('user logged in successfully', authData);
+            cb();
+          } else {
+            console.log('Error creating user:', error);
+          }
+        })
+      }
+
+      factory.registerUser = function(email,password,cb){
+        ref.createUser({
+          email: email,
+          password: password
+        }, function(error,authData){
+          if(error === null){
+            console.log('User created successfully', authData);
+            cb()
+          } else {
+            console.log('Error creating user: ', error);
+          }
+        });
+      }
+
+      factory.forgotPassword = function(email){
+        ref.resetPassword({
+          email : email
+        }, function(error) {
+          if (error === null) {
+            console.log("Password reset email sent successfully");
+          } else {
+            console.log("Error sending password reset email:", error);
+          }
+        })
+      }
+
+      factory.logout = function(cb){
+        ref.unauth(function(){
+          console.log('logout works')
+          cb();
+        });
+      }
+
+      return factory;
+    })
 })();
